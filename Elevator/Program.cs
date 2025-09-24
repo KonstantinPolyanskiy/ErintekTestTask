cfa94162d3b6f747486a524162e89c5fb38d082e
polyanskiy <polyanskiy.main@list.ru>
2025-09-25 00:38:12 +0300

    Выполненная задача 1

--- Elevator/Program.cs
+++ Elevator/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+namespace Elevator;
 
-Console.WriteLine("Hello, World!");
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        var elevator = new Impl.Elevator(26, -3);
+
+        while (true)
+        {
+            Console.Write("Введите номер этажа: ");
+
+            var input = Console.ReadLine();
+            if (input == null)
+            {
+                break;
+            }
+
+            if (!int.TryParse(input, out int floor))
+            {
+                Console.WriteLine("Ошибка ввода, необходимо целое число");
+                continue;
+            }
+
+            elevator.Move(floor);
+        }
+    }
+}
+
+
